af9f47438bf2ea4eaafa4237d0413cc57a01218f
Scott Pullen <s.pullen05@gmail.com>
2014-09-20 18:58:13 -0400

    Adding jquery, some more initialization, filled in more of the readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# CSCI E-15 Project 2
+
+[Visit Site](http://p2.scottpullen.me)
+[Demo](http://)
+
+## Description
+This application generates a password. The password is built using ordinary words (no l337 speak) and some simple options. The generated password is easy to remember, but is hard for a computer to crack. The idea comes from [xkcd](http://xkcd.com/936/).
+
+## Resources
+- [jQuery](http://jquery.com)
+- [Bootstrap](http://getbootstrap.com)
---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+
+  <title>CSCI E-15 Project 2 - Scott Pullen</title>
+
+  <link rel="stylesheet" media="screen" href="css/bootstrap.min.css">
+  <link rel="stylesheet" media="screen" href="css/application.css">
+</head>
+<body>
+
+TODO
+
+<script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/application.js"></script>
+</body>
+</html>

d76084d30c618e0be152969558865949cbfec65c
Scott Pullen <s.pullen05@gmail.com>
2014-09-21 21:54:04 -0400

    adding generate.php to generate json version

--- generate.php
+++ generate.php
@@ -1,3 +1,12 @@
 <?php
+  header('Content-Type: application/json');
 
+  require 'password_generator.php';
+  $pg = new PasswordGenerator($_POST);
+
+  if($pg->isValid()) {
+    echo json_encode($pg->generate());
+  } else {
+    echo json_encode($pg->errors);
+  }
 ?>

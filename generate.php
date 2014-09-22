3b898d79a65f09dac08ef90f84e33263d6d67fa8
Scott Pullen <s.pullen05@gmail.com>
2014-09-21 22:26:26 -0400

    handing json request and response properly

--- generate.php
+++ generate.php
@@ -1,9 +1,12 @@
 <?php
-  header('Content-Type: application/json');
-
   require 'password_generator.php';
-  $pg = new PasswordGenerator($_POST);
 
+  $json = file_get_contents('php://input');
+  $options = json_decode($json, true);
+
+  $pg = new PasswordGenerator($options);
+
+  header('Content-Type: application/json');
   if($pg->isValid()) {
     echo json_encode($pg->generate());
   } else {
